b7baeace965b82f4ee8d7a9af7a5c4e501560cc6
Robert Gabriel Mihai <roby07g@gmail.com>
2025-11-18 22:48:32 +0100

    fix: weighted edge now appears without an arrow pointing to the path it is headed.

--- tasklist-glsp-client/src/views.tsx
+++ tasklist-glsp-client/src/views.tsx
@@ -24,28 +24,10 @@ export class WeightedEdgeView extends PolylineEdgeViewWithGapsOnIntersections {
         const arrow = (
             <path
                 class-sprotty-edge={true}
-                class-arrow={true}
-                d="M 1,0 L 10,-4 L 10,4 Z"
                 transform={`rotate(${toDegrees(angleOfPoint(Point.subtract(p1, p2)))} ${p2.x} ${p2.y}) translate(${p2.x} ${p2.y})`}
             />
         );
         additionals.push(arrow);
-
-        /*const label = edge.children.find(c => c.type === 'label:weighted') as GLabel | undefined;
-            if (label) {
-                const midPoint: Point = { x: (p1.x + p2.x) / 2, y: (p1.y + p2.y) / 2 };
-                additionals.push(
-                    <text
-                        x={midPoint.x}
-                        y={midPoint.y - 5}
-                        text-anchor="middle"
-                        class={{ 'weighted-edge-label': true }}
-                    >
-                        {label.text}
-                    </text>
-                );
-            }*/
-
         return additionals;
     }
 }
